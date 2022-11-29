bb93370045bfa7565bd5d5e465443d351ba59bdc
SalmonFillet <82758161+5h4k3m3@users.noreply.github.com>
2022-11-29 21:12:59 +0900

    🚧 pokemon type

---
+++ src/models/pokemon/pokemon.ts
@@ -0,0 +1,12 @@
+export type Pokemon = {
+  id: number;
+  name: string;
+  nickName: string;
+  gender: string;
+  level: number;
+  types: string[];
+  abilities: string[];
+  character: string;
+  item: string;
+  moves: string[];
+};
